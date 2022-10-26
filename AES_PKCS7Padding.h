b9437f8b1f51c3037e355d175eb6def006b9492b
LeeKasm <43577193+LeeKasm@users.noreply.github.com>
2022-10-26 16:32:28 +0800

    Add AES_PKCS7Padding Algorithm

---
+++ AES_PKCS7Padding.h
@@ -0,0 +1,20 @@
+#ifndef AES_PKCS7PADDING_H
+#define AES_PKCS7PADDING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include "AES.h"
+#include "PKCS7Padding.h"
+
+	bool AES_CBC_PKCS7_ENCRYPT(const char* key, const char* iv, uint8_t* inputBuffer, size_t inputSize, uint8_t* outputBuffer, size_t outputSize, size_t* returnSize);
+	bool AES_CBC_PKCS7_DECRYPT(const char* key, const char* iv, uint8_t* inputBuffer, size_t inputSize, uint8_t* outputBuffer, size_t outputSize, size_t* returnSize);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* AES_PKCS7PADDING_H */
